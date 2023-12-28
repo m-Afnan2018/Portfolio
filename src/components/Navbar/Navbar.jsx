960c1cea17c761995bc511cb4b0021b4d7b74b0d
m.Afnan <m.afnan2018@gmail.com>
2023-12-28 22:54:48 +0000

    Responsive Navbar TBC...

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './Navbar.module.css'
 import lettera from '../../assets/images/letter-a.png'
 import letterm from '../../assets/images/letter-m.png'
 import { Link } from 'react-scroll'
+import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
+    const [showOverLay, setShowOverLay] = useState(false)
     return (
         <div className={style.Navbar}>
             <div className={style.Name}>
                 <div>
-                    <img src={letterm} />
+                    <img src={letterm} alt='M' />
                     <p>ohammad</p>
                 </div>
                 <div>
-                    <img src={lettera} />
+                    <img src={lettera} alt='A' />
                     <p>fnan</p>
                 </div>
             </div>
-            <div className='Nav'>
+            <div className={style.Nav}>
                 <Link to="projects" smooth={true} duration={500}>
                     Projects
                 </Link>
@@ -28,16 +30,22 @@ const Navbar = () => {
                     Contact me
                 </Link>
             </div>
-            <div className='Nav-Overlay'>
-                <Link to="projects" smooth={true} duration={500}>
-                    Projects
-                </Link>
-                <Link to="techStack" smooth={true} duration={500}>
-                    Skills
-                </Link>
-                <Link to="contactUs" smooth={true} duration={500}>
-                    Contact me
-                </Link>
+            <div className={style.NavOverlay}>
+                <FiMenu onClick={() => setShowOverLay(!showOverLay)} />
+                {
+                    showOverLay &&
+                    <div>
+                        <Link to="projects" smooth={true} duration={500}>
+                            Projects
+                        </Link>
+                        <Link to="techStack" smooth={true} duration={500}>
+                            Skills
+                        </Link>
+                        <Link to="contactUs" smooth={true} duration={500}>
+                            Contact me
+                        </Link>
+                    </div>
+                }
             </div>
         </div>
     )
