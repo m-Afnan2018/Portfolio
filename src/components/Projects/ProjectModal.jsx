b9e726845a3b785544b9c281769b9fb3a4126c31
m.Afnan <m.afnan2018@gmail.com>
2024-01-07 16:26:59 +0000

    Working

--- src/components/Projects/ProjectModal.jsx
+++ src/components/Projects/ProjectModal.jsx
@@ -9,41 +9,53 @@ const ProjectModal = ({ data, setShow }) => {
     return (
         <div className={style.ProjectModal}>
             <div className={style.image}>
-                <img src={image} alt='mainImage'/>
+                <img src={image} alt='mainImage' />
                 <div className={style.thumbnail}>
-                    { 
-                        data.moreImages.map((i)=>(
-                            <img src={i} onClick={()=>selectImage(i)} alt='thumbnail'/>
+                    {
+                        data.moreImages.map((i) => (
+                            <img src={i} onClick={() => selectImage(i)} alt='thumbnail' />
                         ))
                     }
                 </div>
             </div>
             <div className={style.information}>
-                <h1>{data.name}</h1>
-                <h2>{data.description}</h2>
-                <h3>Features: </h3>
-                <ul>
-                    {
-                        data.points.map((point) => (
-                            <li>{point}</li>
-                        ))
-                    }
-                </ul>
-                <h3>Tech Stack: </h3>
                 <div>
-                    {
-                        data.techStack.map((tech)=>(
-                            <div>{tech}</div>
-                        ))
-                    }
+
+                    <h1>{data.name}</h1>
+                    <h2>{data.description}</h2>
                 </div>
-                <h3>Links: </h3>
                 <div>
-                    <FaGithub/>
-                    <FaYoutube/>
-                    <FaLink/>
+
+                    <h3>Features: </h3>
+                    <ul>
+                        {
+                            data.points.map((point) => (
+                                <li>{point}</li>
+                            ))
+                        }
+                    </ul>
                 </div>
-                <IoCloseCircleOutline className={style.cross} onClick={()=>setShow(false)}/>
+                <div>
+                    <h3>Tech Stack: </h3>
+                    <div className={style.tech}>
+                        {
+                            data.techStack.map((tech) => (
+                                <div>{tech}</div>
+                            ))
+                        }
+                    </div>
+
+                </div>
+                <div>
+                    <h3>Links: </h3>
+                    <div className={style.links}>
+                        <FaGithub />
+                        <FaYoutube />
+                        <FaLink />
+                    </div>
+
+                </div>
+                <IoCloseCircleOutline className={style.cross} onClick={() => setShow(false)} />
             </div>
         </div>
     )
